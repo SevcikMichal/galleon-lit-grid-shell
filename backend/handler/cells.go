85b91702164bcbc2d4d80318c0499c45fc0ed3aa
Michal Sevcik <michal.sevcik@protonmail.com>
2026-03-31 21:21:56 +0200

    Add dispatch and fix lint issue

--- backend/handler/cells.go
+++ backend/handler/cells.go
@@ -38,7 +38,7 @@ func setCORSHeaders(w http.ResponseWriter) {
 func writeJSON(w http.ResponseWriter, status int, v interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(v)
+	_ = json.NewEncoder(w).Encode(v)
 }
 
 func writeError(w http.ResponseWriter, status int, msg string) {
